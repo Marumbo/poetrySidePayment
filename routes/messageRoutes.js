7994f74d751d05e7ed874738864db20ad7ba3a52
Marumbo <marumbok@gmail.com>
2023-09-20 18:19:21 +0200

    added sms model and function in controller to send

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,18 +1,21 @@
-const express = require('express')
+const express = require("express");
 
-const { message_index, message_create_post} = require('../controllers/messageController')
+const {
+  message_index,
+  message_create_post,
+  send_sms,
+} = require("../controllers/messageController");
 
-
-const router = express.Router()
-
+const router = express.Router();
 
 // get all messages
 
-router.get('/all', message_index)
+router.get("/all", message_index);
 
 // add message
 
-router.post('/create', message_create_post)
- 
- module.exports = router;
+router.post("/create", message_create_post);
 
+router.post("/sendSMS", send_sms);
+
+module.exports = router;
